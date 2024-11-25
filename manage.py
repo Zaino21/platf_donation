a9be55353096448de31c25d214bc47fc0799d6d8
Matheus Zaino <matheuszainopo@gmail.com>
2024-11-25 11:45:42 -0300

    parte 2 da aula de 25/11

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pweb2_trabalho.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pweb2trabalho.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
---
+++ pweb2_trabalho/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Pweb2TrabalhoConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'pweb2_trabalho'
